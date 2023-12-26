2dbfca6390d0f44521b54603242dd392a1ad006d
dan443322 <davearlunino.tindoy@ctu.edu.ph>
2023-12-26 17:03:52 +0800

    12312

--- scripts/navEffects.js
+++ scripts/navEffects.js
@@ -6,17 +6,15 @@ const elementsToBlur = document.querySelectorAll(
   ".home-container > :not(.home-navbar)"
 );
 
-console.log(burgerBtn);
-
-let isImage1 = true;
+var isImage1 = true;
 
 function toggleImageAndDiv() {
   // Toggle the image source
   burgerBtn.querySelector("img").src = isImage1
     ? "resources/Icons/close-bold-svgrepo-com.svg"
-    : "resources/Icons/menu-burger-horizontal-svgrepo-com.svg"; // Replace with the path to your other image
+    : "resources/Icons/menu-burger-horizontal-svgrepo-com.svg";
 
-  // Toggle the visibility of another div (replace '.mobile-links' with your actual class)
+  // Toggle the visibility of another div
 
   // Toggle fade-in and fade-out effect with move from right animation
   if (otherDiv.style.display === "none" || otherDiv.style.display === "") {
@@ -24,24 +22,43 @@ function toggleImageAndDiv() {
     setTimeout(() => {
       otherDiv.classList.remove("fade-out");
       otherDiv.classList.add("fade-in");
+    }, 10);
+    setTimeout(() => {
       elementsToBlur.forEach((element) => {
-        // Add filter blur 5px to each selected element
+        // Add filter blur
         element.style.filter = "blur(5px)";
       });
-    }, 10);
+    }, 500);
   } else {
     otherDiv.classList.remove("fade-in");
     otherDiv.classList.add("fade-out");
 
     setTimeout(() => {
       elementsToBlur.forEach((element) => {
-        // Add filter blur 5px to each selected element
+        // Add filter blur
         element.style.filter = "blur(0px)";
       });
       otherDiv.style.display = "none";
     }, 500);
   }
 
-  // Update the state of the image
+  // Updates the state of the image
   isImage1 = !isImage1;
+}
+
+function afterNavClick() {
+  isImage1 = true;
+  burgerBtn.querySelector("img").src =
+    "resources/Icons/menu-burger-horizontal-svgrepo-com.svg";
+
+  otherDiv.classList.remove("fade-in");
+  otherDiv.classList.add("fade-out");
+
+  setTimeout(() => {
+    elementsToBlur.forEach((element) => {
+      // Add filter blur
+      element.style.filter = "blur(0px)";
+    });
+    otherDiv.style.display = "none";
+  }, 500);
 }
